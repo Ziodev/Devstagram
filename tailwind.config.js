71f22050d1e35a27953d767ee2aeef73e6d3ec03
Rafael J. Ortiz Sanchez <30805105+Ziodev@users.noreply.github.com>
2024-07-02 16:08:27 -0400

    Register view and controller

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+      "./resources/views/**/*.blade.php",
+      "./resources/views/**/*.blade.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
